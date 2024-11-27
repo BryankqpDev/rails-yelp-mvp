815f86d8691d2508deb4c2daae10ccf157c42553
Bryan Quispe <bryankqp@gmail.com>
2024-11-27 11:47:19 -0500

    Yelp MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
+  def new
+    @review = @restaurant.reviews.new
+  end
+
+  def create
+    @review = @restaurant.reviews.new(review_params)
+    if @review.save
+      redirect_to @restaurant, notice: 'Review was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #   resources :restaurants
+
+  Rails.application.routes.draw do
+    resources :restaurants do
+      resources :reviews, only: [:new, :create]
+    end
+  end
 end
